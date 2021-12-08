fdd39bd258989b22f83daec412063dd54b85a317
Cornelia Gherman <70709834+corneliag08@users.noreply.github.com>
2021-12-08 20:58:21 +0200

    Update aplicatie practica.py

--- aplicatie practica.py
+++ aplicatie practica.py
@@ -1,29 +1,16 @@
-with open('C:\Users\Statie-13-C100\Desktop\Lista clasei 11D.txt',encoding='utf-8-sig') as f :
-    linii=f.read()
-with open('C:\Users\Statie-13-C100\Desktop\Lista clasei 11D.txt',mode='rt',encoding='utf-8-sig') as f :
-    linii2=list(linii)
-import statistics
-print ("Numele Prenumele Nota")
-print(linii)
-linii2=str(linii)
-notele=[]
-for i in linii :
-    if i.isnumeric() and i!="1" and i!="0":
-        notele.append(i)
-    if i=="1":
-        notele.append("10")
-x=[int(u) for u in notele]
-med=statistics.mean(x)
-print("Nota medie este :",med)
-with open('C:\Users\Statie-13-C100\Desktop\Limba engleza grupa 1.txt','w',encoding='utf-8-sig') as f :
-    for linie in l:
-        m=linie.split()
-        if m[3]=='Engl1'
-        f.write(linie)
-    f.close()
-with open('C:\Users\Statie-13-C100\Desktop\Limba engleza grupa 2.txt','w',encoding='utf-8-sig') as f :
-    for linie in l:
-        m=linie.split()
-        if m[3]=='Engl2':
-            f.write(linie)
-        f.close
+with open('desktop/INPUT.txt','r') as f:
+    list1=f.readlines()
+print('Nr.	Numele	Prenumele	Nota1	Nota2	Nota3\n',*list1)
+with open('desktop/REZERVA.txt','w') as f:
+    for i in list1:
+        f.write(i)
+for i in list1:
+    list2=i.split()
+    x=list2[0]+' '+list2[1]+' '+list2[2]
+    media=str((float(list2[3])+float(list2[4])+float(list2[5]))/3)
+    y=x+' '+media+'\n'
+    with open('desktop/OUTPUT.txt','a') as f:
+        f.write(y)
+with open('desktop/OUTPUT.txt','r') as f:
+    list3=f.readlines()
+print('Nr.	Numele	Prenumele	media',*list3)
