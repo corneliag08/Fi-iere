aef28f5da1551ab84536b40560bb8d4367daf437
Cornelia Gherman <70709834+corneliag08@users.noreply.github.com>
2021-12-08 21:08:48 +0200

    Update aplicatie practica.py

--- aplicatie practica.py
+++ aplicatie practica.py
@@ -1,16 +1,2 @@
-with open('C:\Users\User\Py\.txt','r') as f:
-    lista_clasei=f.readlines()
-print('Nr.   Numele  Prenumele	Nota     Grupa\n',*list1)
-with open('C:\Users\User\Py\.txt', 'w') as f:
-    for i in lista_clasei:
-        f.write(i)
-for i in lista_clasei:
-    lista=i.split()
-    x=lista[0]+' '+lista[1]+' '+lista[2]
-    media=str((float(lista[3])+float(lista[4])+float(lista[5]))/3)
-    z=x+' '+media+'\n'
-    with open('C:\Users\User\Py\.txt','a') as f:
-        f.write(z)
-with open('C:\Users\User\Py\.txt','r') as f:
-    listaf=f.readlines()
-print('Nr.	Numele	Prenumele	media',*listaf)
+with open('Lista_clasei.txt','rt') as f:
+    l=list(f)
