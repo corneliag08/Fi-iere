fc1a90cb96b50d5444f95352bcceac1e663a976c
Cornelia Gherman <70709834+corneliag08@users.noreply.github.com>
2021-12-08 22:16:56 +0200

    Update aplicatie practica.py

--- aplicatie practica.py
+++ aplicatie practica.py
@@ -1,2 +1,15 @@
-with open('Lista_clasei.txt','rt') as f:
-    l=list(f)
+with open('C:\Users\User\Py\Lista_clasei.txt','r') as f:
+   x=list(f)
+   f.close()
+print('         Nume','Prenume','Nota','Grupa 1/2',sep='\t')
+for i,linie  in enumerate(x):
+    print(i+1,':\t',linie,end='')
+z=media=0   
+for linie in x:
+    campuri=linie.split()
+    nota=int(campuri[2])
+    z+=1
+    media+=nota
+s1=s2=0
+print(nota,sep='\t')
+print('Media este',z,media/float(n))
